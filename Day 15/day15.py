9570e55bd0eb388cb5b818dbfd22b8ac2c8718a8
Antonio Tigri <entertainmentatv@gmail.com>
2022-12-16 00:35:55 +0100

    Day 15 - Part 2

--- Day 15/day15.py
+++ Day 15/day15.py
@@ -1,10 +1,8 @@
 
 def get_report(file):
-
     report = dict()
 
     for line in open(file).read().splitlines():
-
         sensor_slice, beacon_slice = line.split(':')
         x_sensor, y_sensor = sensor_slice[10:].split(', ')
         x_beacon, y_beacon = beacon_slice[22:].split(', ')
@@ -66,6 +64,90 @@ def visualize_report(report):
     f.close()
 
 
+def add_interval(intervals, new_interval, start, end):
+    if not intervals:
+        i_start, i_end = new_interval
+        if i_end >= start:
+            i_start = start if i_start < start else i_start
+            if i_start <= end:
+                i_end = end if i_end > end else i_end
+                intervals.append((i_start, i_end))
+    else:
+        i_start, i_end = new_interval
+        if i_end >= start:
+            i_start = start if i_start < start else i_start
+            if i_start <= end:
+                i_end = end if i_end > end else i_end
+                intervals.append((i_start, i_end))
+                intervals = fix_overlaps(intervals)
+    return intervals
+
+
+def fix_overlaps(intervals):
+    fixed_intervals = list()
+    current_min = float('inf')
+    current_min_idx = 0
+    for i, interval in enumerate(intervals):
+        if interval[0] < current_min:
+            current_min = interval[0]
+            current_min_idx = i
+    interval_start, interval_end = intervals.pop(current_min_idx)
+    while intervals:
+        interval_start, interval_end = fix_first_component(intervals, fixed_intervals, interval_start, interval_end)
+    fixed_intervals.append((interval_start, interval_end))
+    return fixed_intervals
+
+
+def fix_first_component(intervals, fixed_intervals, interval_start, interval_end):
+
+    if not intervals:
+        return interval_start, interval_end
+
+    current_min = float('inf')
+    current_min_idx = 0
+    for i, interval in enumerate(intervals):
+        if interval[0] < current_min:
+            current_min = interval[0]
+            current_min_idx = i
+    if current_min <= interval_end:
+        if intervals[current_min_idx][1] > interval_end:
+            interval_end = intervals[current_min_idx][1]
+            intervals.pop(current_min_idx)
+            return fix_second_component(intervals, fixed_intervals, interval_start, interval_end)
+        else:
+            intervals.pop(current_min_idx)
+            return fix_first_component(intervals, fixed_intervals, interval_start, interval_end)
+    else:
+        fixed_intervals.append((interval_start, interval_end))
+        interval_start, interval_end = intervals.pop(current_min_idx)
+        return interval_start, interval_end
+
+
+def fix_second_component(intervals, fixed_intervals, interval_start, interval_end):
+
+    if not intervals:
+        return interval_start, interval_end
+
+    current_min = float('inf')
+    current_min_idx = 0
+    for i, interval in enumerate(intervals):
+        if interval[1] < current_min:
+            current_min = interval[1]
+            current_min_idx = i
+    if current_min <= interval_end:
+        intervals.pop(current_min_idx)
+        return fix_second_component(intervals, fixed_intervals, interval_start, interval_end)
+    else:
+        if intervals[current_min_idx][0] <= interval_end:
+            intervals.pop(current_min_idx)
+            interval_end = current_min
+            return fix_first_component(intervals, fixed_intervals, interval_start, interval_end)
+        else:
+            fixed_intervals.append((interval_start, interval_end))
+            interval_start, interval_end = intervals.pop(current_min_idx)
+            return interval_start, interval_end
+
+
 def part1(report, row):
     discarded_positions = set()
     sensors_set = set(report.keys())
@@ -79,12 +161,35 @@ def part1(report, row):
     return len(discarded_positions.difference(sensors_set.union(beacons_set)))
 
 
-def part2(report):
-    pass
+def part2(report, start, end):
+    from tqdm import tqdm
+
+    sensors_set = set(report.keys())
+    found_beacon_spot = False
+    x_beacon, y_beacon = 0, 0
+    for row in tqdm(range(start, end)):
+        discarded_ranges = list()
+        if found_beacon_spot:
+            break
+        for sensor in sensors_set:
+            distance = report[sensor][1]
+            if sensor[1] + distance >= row or sensor[1] - distance <= row:
+                rel_distance = distance - abs(row - sensor[1])
+                discarded_ranges = add_interval(discarded_ranges, (sensor[0] - rel_distance,
+                                                                   sensor[0] + rel_distance), start, end)
+        prev_range_end = discarded_ranges[0][1]
+        for _range in discarded_ranges[1:]:
+            if prev_range_end + 1 != _range[1]:
+                x_beacon, y_beacon = _range[0] - 1, row
+                found_beacon_spot = True
+                break
+
+    return x_beacon * end + y_beacon
 
 
 _report = get_report('input.txt')
 _row = 2000000
-#visualize_report(_report)
-#print('In the row', _row, 'there are', part1(_report, _row), 'positions where a beacon cannot be present.')
-part2(_report)
+# The visualize method is just for show with small inputs
+# visualize_report(_report)
+print('In the row', _row, 'there are', part1(_report, _row), 'positions where a beacon cannot be present.')
+print('The tuning frequency is', part2(_report, 0, 4000000))
